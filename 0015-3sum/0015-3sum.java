4553258bb48fa6d9408717e323c6cc5ae375ea6c
Anant Sharma <anant.sharma05@hotmail.com>
2026-09-07 10:45:46 +0530

    Time: 862 ms (9.47%), Space: 59.6 MB (20.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,55 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> arr = new ArrayList<>();
-
+        Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate first numbers
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    arr.add(Arrays.asList(
-                        nums[i],
-                        nums[left],
-                        nums[right]
-                    ));
-
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     left++;
                     right--;
-
-                    // Skip duplicate left values
-                    while (left < right &&
-                           nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right values
-                    while (left < right &&
-                           nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-
-                } else if (sum < 0) {
+                } else if (sum<0){
                     left++;
                 } else {
                     right--;
                 }
             }
         }
-
-        return arr;
+        return new ArrayList<>(result);
     }
 }
